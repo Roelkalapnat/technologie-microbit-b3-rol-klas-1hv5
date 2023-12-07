07e31648e64981435c034e1db06ba3d93cce6b26
Roelkalapnat <152483260+Roelkalapnat@users.noreply.github.com>
2023-12-07 09:41:26 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,60 @@
+let dobbelsteen = 0
+input.onGesture(Gesture.Shake, function () {
+    dobbelsteen = randint(1, 6)
+})
+basic.forever(function () {
+    while (dobbelsteen == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (dobbelsteen == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    while (dobbelsteen == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    while (dobbelsteen == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    while (dobbelsteen == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    while (dobbelsteen == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
